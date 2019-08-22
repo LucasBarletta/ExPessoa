b47bb3d259ee2135b3e5927f9ce5bb0157c0eba3
Lucas Munhoz <52502481+LucasBarletta@users.noreply.github.com>
2019-08-22 16:54:49 -0300

    Corrigir prints

--- ExClasse.py
+++ ExClasse.py
@@ -3,15 +3,15 @@ class Pessoa:
         self.nome = nome
         self.cpf = cpf
 
-        print(nome,cpf)
+
 
 class Paciente(Pessoa):
     def __init__(self, nome, cpf, sintoma):
         super().__init__(nome, cpf)
         self.sintoma = sintoma
 
-    def sintoma(self):
-        print('Gripe')
+
+        print(nome,cpf,sintoma)
 
 class Medico(Pessoa):
 
@@ -19,12 +19,8 @@ class Medico(Pessoa):
         super().__init__(nome, cpf)
         self.crm = crm
 
-    def crm(self):
-        print('Numero crm')
+
+        print(nome, cpf,crm)
 
 paciente = Paciente('Joao','123456789', 'gripe')
 medico = Medico('Jose', '98765413', 'crm')
-pessoa = Pessoa('nome', 'cpf')
-
-print(paciente.sintoma)
-print(medico.crm)
